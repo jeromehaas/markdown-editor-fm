f9aab0c257d4b9a040b930cf7fa19f4d74f048a4
jeromehaas <hello@jeromehaas.ch>
2022-06-27 22:04:18 +0200

    Refactor: switch from MongoDB to localstorage

--- data/welcome.js
+++ data/welcome.js
@@ -3,8 +3,7 @@ import moment from 'moment';
 const welcome = {
   id: '9999999',
   filename: 'welcome.md',
-  content: `
-  # Welcome to Markdown
+  content: `# Welcome to Markdown
 
   Markdown is a lightweight markup language that you can use to add formatting elements to plaintext text documents.
   
